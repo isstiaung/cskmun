bc46106a1f623aff98576c328f02f3909fd4d88b
Sainath Rajagopal <hello@isstiaung.me>
2019-05-17 17:55:42 +0530

    replaced old skeleton

--- py/config_skeleton.py
+++ py/config_skeleton.py
@@ -1,9 +1,13 @@
 csk_dir = ''
 mun_dir = ''
-
+unified_dir = ''
+unified_file = 'unified_data.csv'
+data_unification=False
 draw = "#ffffdd"
 loss = "#ffdddd"
 win = "#ffdddd"
+
+csvs = "./output_*.csv"
 
 lxml = "lxml"
 
@@ -17,17 +21,35 @@ mun_table_type = "class"
 mun_table_attrs = "wikitable sortable"
 mun_link_type = "class"
 mun_link_attrs = "external text"
+mun_span_type = "id"
+mun_span_attrs = "Matches"
+mun_span_alt_attrs = "Premier_League"
 mun_bgcolor = "bgcolor"
-mun_date_column = 'Date'
-combined_mun = "combined_mun.csv"
+unified_date_column = 'Date'
+combined_mun_file ="combined_mun.csv"
+mun_filler = '/output_mun_'
+mun_to_drop = ["Attendance","Leagueposition","Leagueposition[23]","Leagueposition[27]","Scorers"]
 
 csk_link_type = "class"
 csk_link_attrs = "QuoteSummary"
-csk_date_column = "Match Date"
-combined_csk = "combined_csk.csv"
+default_date_column = "Match Date"
+combined_csk_file = "combined_csk.csv"
+csk_to_drop = ["Margin","Scorecard"]
+csk_filler = '/output_csk_'
 
+new_column = 'played_first'
+default_column_value = 'csk'
 ctable = "table"
 ctable_row = "tr"
 ctable_header = "th"
 clinks = 'a'
 chref = 'href'
+span = "span"
+c_sep= ","
+csv = ".csv"
+utf8 = "utf-8"
+t_win = "win"
+t_draw = "draw"
+t_loss = "loss"
+mun_text = "United"
+csk_text = "CSK"
