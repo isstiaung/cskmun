2016aae33a0c563dc914b183fa71b535cd77986b
Sainath Rajagopal <hello@isstiaung.me>
2019-05-17 15:39:17 +0530

    combine files fix

--- combine_files.py
+++ combine_files.py
@@ -1,14 +1,16 @@
 import pandas as pd
 import glob,os
-import config
+from config import *
 
+def combine_files():
+    os.chdir(csk_dir)
+    csk_files = glob.glob("./*.csv")
+    combined_csk = pd.concat( [ pd.read_csv(f) for f in csk_files ] )
+    combined_csk.to_csv( "combined_csk.csv", index=False )
 
-os.chdir(config.csk_dir)
-csk_files = glob.glob("./*.csv")
-combined_csk = pd.concat( [ pd.read_csv(f) for f in csk_files ] )
-combined_csk.to_csv( "combined_csk.csv", index=False )
+    os.chdir(mun_dir)
+    mun_files = glob.glob("./*.csv")
+    combined_mun = pd.concat( [ pd.read_csv(f) for f in mun_files ] )
+    combined_mun.to_csv( "combined_mun.csv", index=False )
 
-os.chdir(config.mun_dir)
-mun_files = glob.glob("./*.csv")
-combined_mun = pd.concat( [ pd.read_csv(f) for f in mun_files ] )
-combined_mun.to_csv( "combined_mun.csv", index=False )
+combine_files()
