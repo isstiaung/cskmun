2c493eaf3c1069b7cfe34c4d4f8b8e62984b689b
Sainath Rajagopal <hello@isstiaung.me>
2019-05-27 17:39:37 +0530

    some manual work now required, followed by code cleanup. too many strings

---
+++ py/get_result.py
@@ -0,0 +1,24 @@
+import pandas as pd
+from utils import *
+from config import *
+
+def get_result():
+    os.chdir(unified_dir)
+    unified_data = read_panda_csv(unified_file,False)
+    unified_data['mun_result']='w'
+    unified_data['csk_result']='w'
+    for index,row in unified_data.iterrows():
+        mun_winner = row['mun_winner']
+        csk_winner = row['csk_winner']
+        final_score = mun_winner.split("(")[0]
+        mun_goals = final_score.split(':')[0]
+        opp_goals = final_score.split(':')[1].strip()
+        if mun_goals == opp_goals:
+            result = 'd'
+        elif mun_goals < opp_goals:
+            result = 'l'
+        unified_data.at[index,'mun_result'] = result
+        if csk_winner != 'Super Kings':
+            unified_data.at[index,'csk_result'] = 'l'
+
+get_result()
--- py/unify.py
+++ py/unify.py
@@ -12,21 +12,17 @@ from config import *
 #     combine_files()
 #     clean_csvs()
 
+def rename_unified_columns(unified_frame):
+    unified_frame.rename(columns = {"Results" : "mun_winner"},inplace=True)
+    unified_frame.rename(columns = {"Winner" : "csk_winner"},inplace=True)
+
 def unify_data():
     combined_mun_frame = get_combined_file(mun_dir,combined_mun_file,True)
     combined_csk_frame = get_combined_file(csk_dir,combined_csk_file,False)
-    print combined_csk_frame
-    print combined_mun_frame
-    print unified_date_column
     unified_frame = pd.merge(combined_mun_frame,combined_csk_frame, on=unified_date_column)
     unified_frame[new_column]=default_column_value
+    rename_unified_columns(unified_frame)
     os.chdir(unified_dir)
     write_panda_to_csv(unified_frame,unified_file)
     print "Done unifying data"
-    print read_panda_csv(unified_file,True)
-
-if data_unification:
-    #setup()
-    unify_data()
-else:
-    print "Get to work"
+    csv_text = read_panda_csv(unified_file,False)
